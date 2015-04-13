452fee8b3fd88cf5902164c20befba1ded20feca
Chinseok Lee <allieuslee@gmail.com>
2015-04-14 01:08:17 +0900

    blog urls 에서 views/views_class 분기할 수 있도록 주석처리

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, url
 
 
 urlpatterns = patterns('blog.views',
+# urlpatterns = patterns('blog.views_class',
+
     url(r'^$', 'index', name='index'),  # noqa
     url(r'^new/$', 'new', name='new'),
     url(r'^(?P<id>\d+)/$', 'detail', name='detail'),
